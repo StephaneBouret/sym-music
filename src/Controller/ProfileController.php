a71e8d0baefa9033328d8ea683a6fd4290cf7457
Stéphane BOURET <sbouret@orange.fr>
2024-12-21 17:42:15 +0100

    Update page modification profil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UpdatePasswordUserFormType;
 use App\Form\UpdateUserFormType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,7 +17,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -36,8 +38,58 @@ class ProfileController extends AbstractController
             return $this->redirectToRoute('app_profile');
         }
 
-        return $this->render('profile/index.html.twig', [
+        return $this->render('profile/edit.html.twig', [
             'form' => $form,
+            'user' => $user
         ]);
+    }
+
+    #[Route('/profile/editPassword', name: 'app_edit_password')]
+    public function editPassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $em)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $this->denyAccessUnlessGranted('CAN_EDIT', $user, 'Vous n\'avez pas confimé votre email');
+
+        $form = $this->createForm(UpdatePasswordUserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $newPassword = $form->get('newPassword')->getData();
+                $password = $userPasswordHasher->hashPassword($user, $newPassword);
+    
+                $user->setPassword($password);
+                $em->flush();
+    
+                $this->addFlash('success', 'Votre mot de passe a bien été mis à jour');
+                return $this->redirectToRoute('app_home');
+            } else {
+                // En cas d'erreurs de validation, rediriger vers la même page
+                $this->addFlash('warning', 'Une erreur est survenue !');
+                return $this->redirectToRoute('app_edit_password');
+            }
+        }
+    
+        return $this->render('profile/credentials.html.twig', [
+            'form' => $form,
+            'user' => $user
+        ]);
+    }
+
+    #[Route('profile/user/{id}/delete', name: 'app_user_delete')]
+    public function delete(Request $request, User $user, UserRepository $userRepository)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $this->denyAccessUnlessGranted('CAN_EDIT', $user, 'Vous n\'avez pas confimé votre email');
+
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $this->container->get('security.token_storage')->setToken(null);
+            $userRepository->remove($user, true);
+            $this->addFlash('success', 'Votre compte a bien été supprimé !');
+        }
+
+        return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
     }
 }
