ba0adff661a7c2762a851b576c05e595ed00b790
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-01-03 15:51:47 +0100

    Mise en place Page programmation

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -21,7 +21,7 @@ class DashboardController extends AbstractDashboardController
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        return $this->redirect($adminUrlGenerator->setController(GenreCrudController::class)->generateUrl());
+        return $this->redirect($adminUrlGenerator->setController(ArtistCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/programmation', name: 'app_program')]
-    public function index(): Response
+    public function index(ArtistRepository $artistRepository): Response
     {
+        $artists = $artistRepository->findAll();
+
         return $this->render('program/index.html.twig', [
-            'controller_name' => 'ProgramController',
+            'artists' => $artists,
         ]);
     }
 }
