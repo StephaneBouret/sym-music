5b813998d50df04df2de27fe5ad0bd6a05ffc7b7
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-01-29 16:02:19 +0100

    Mise en place d'un EventSubscriber

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,12 +4,14 @@ namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
 use App\Cart\CartService;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class PurchasePaymentSuccessController extends AbstractController
 {
@@ -18,7 +20,7 @@ class PurchasePaymentSuccessController extends AbstractController
 
     #[Route('/purchase/terminate/{id}', name: 'app_payment_success')]
     #[IsGranted('ROLE_USER')]
-    public function success($id, PurchaseRepository $purchaseRepository): Response
+    public function success($id, PurchaseRepository $purchaseRepository, EventDispatcherInterface $dispatcher): Response
     {
         $user = $this->getUser();
         $this->denyAccessUnlessGranted('CAN_EDIT', $user, 'Vous n\'avez pas confimé votre email');
@@ -38,6 +40,11 @@ class PurchasePaymentSuccessController extends AbstractController
         $this->em->flush();
         // 3. Je veux vider le panier
         $this->cartService->empty();
+
+        // 3.1 Lancer un événement qui permet de réagir à la prise d'une commande (ex. envoi de mail)
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
+        
         // 4. Redirection avec un addflash vers la liste des commandes
         $this->addFlash("success", "La commande a été payée et confirmée !");
         return $this->redirectToRoute('app_purchase_list');
